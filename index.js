52670f07675be9dfdd0d5d10867e3d273d15d0c0
Martin <martinigonsalves@gmail.com>
2020-09-25 17:58:30 +0200

    upload popup

---
+++ index.js
@@ -0,0 +1,6 @@
+const nameMovie = "pulp fiction";
+const year = 1994;
+const nameDirector = "Quentin Tarantino";
+const message = `${nameMovie}, directed by ${nameDirector}, was released in ${year}`;
+
+alert(message);
